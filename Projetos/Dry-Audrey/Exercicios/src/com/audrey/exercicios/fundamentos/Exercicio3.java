afa0ab4ca5f0cb38146d719855020a1df7ddb46c
Audrey-Dev-Full-Stack <audreydev.br@gmail.com>
2024-08-25 22:50:04 -0300

    Adicionando exxercicio 3

---
+++ Projetos/Dry-Audrey/Exercicios/src/com/audrey/exercicios/fundamentos/Exercicio3.java
@@ -0,0 +1,31 @@
+package com.audrey.exercicios.fundamentos;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int cavalos;
+        float valorUnitFerradura;       
+        
+        
+        System.out.println("Informe a quantidade de cavalos: ");
+        cavalos = leia.nextInt();
+
+        int qtddFerraduras = cavalos * 4;
+
+        System.out.println("Informe o valor de cada ferradura: ");
+        valorUnitFerradura = leia.nextFloat();
+
+        float valorTotalFerradura = valorUnitFerradura * qtddFerraduras;
+
+
+        System.out.println("A quantidade de ferraduras necessarias: "+ qtddFerraduras);
+        System.out.printf("Valor total para a compra das ferraduras é : R$ %.2f ", valorTotalFerradura );
+
+    }
+
+    
+}
